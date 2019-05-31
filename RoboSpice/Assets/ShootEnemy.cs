0b6dd18779715641c6c0ff9ebb2ea9f133d2f6ad
JOHNDHOPKINS <45177624+JOHNDHOPKINS@users.noreply.github.com>
2019-05-31 13:54:56 -0400

    shooter

---
+++ RoboSpice/Assets/ShootEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemy : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject projectTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots<=0)
+        {
+            Instantiate(projectTile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+        
+    }
+}
